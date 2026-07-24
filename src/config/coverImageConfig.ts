c5d49ce94e9eeba57526b910a02f0c610620c475
xiaye <xiaye@msn.com>
2026-07-24 21:59:16 +0800

    feat: 优化文章详情页封面与元数据展示

--- src/config/coverImageConfig.ts
+++ src/config/coverImageConfig.ts
@@ -4,6 +4,7 @@ import type { CoverImageConfig } from "../types/coverImageConfig";
  * 文章封面图配置
  *
  * enableInPost - 是否在文章详情页显示封面图
+ * showLoading - 是否显示封面图加载动画
  *
  * 随机封面图使用说明：
  * 1. 在文章的 Frontmatter 中添加 image: "api" 即可使用随机图功能
@@ -19,6 +20,9 @@ export const coverImageConfig: CoverImageConfig = {
 	// 是否在文章详情页显示封面图
 	enableInPost: true,
 
+	// 是否显示转圈圈加载动画，会替代掉LQIP
+	showLoading: false,
+
 	randomCoverImage: {
 		// 随机封面图功能开关
 		enable: false,
@@ -30,7 +34,5 @@ export const coverImageConfig: CoverImageConfig = {
 		],
 		// API失败时的回退图片路径（相对于src目录或以/开头的public目录路径）
 		fallback: "assets/images/cover.avif",
-		// 是否显示加载动画
-		showLoading: false,
 	},
 };
